e8a0430f8f4be7d942726f22853ef8d07e55f128
Nghia <nghianguyentin99@gmail.com>
2025-10-23 16:27:42 +0700

    create login screen

---
+++ Repository/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using PCShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCShop.Repository
+{
+    public class UserRepository
+    {
+        private readonly PcshopDbContext _context;
+
+        public UserRepository()
+        {
+            _context = new PcshopDbContext();
+        }
+
+        public User? Login(string username, string password)
+        {
+            return _context.Users
+                .FirstOrDefault(u => u.Username == username && u.Password == password);
+        }
+    }
+}
