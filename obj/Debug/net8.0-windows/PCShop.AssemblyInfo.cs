98e5590f04179d0915b56d460f7c3d03d1924c00
PhamSonHaDo1612 <105654183+sonha1612@users.noreply.github.com>
2025-10-24 15:01:26 +0700

    merge with main

--- obj/Debug/net8.0-windows/PCShop.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/PCShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PCShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5ecaae40bedd0f086a2f9c61a6492e3c1c4b60a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0986253a297accf19c085990887f11500a000b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PCShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PCShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
